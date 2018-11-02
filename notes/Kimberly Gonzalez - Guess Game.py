0488bde2cdbe62d1605990b2c9db5c3bb6fec486
Xena774 <42391953+Xena774@users.noreply.github.com>
2018-11-02 14:13:15 -0700

    Worked on guess game

--- notes/Kimberly Gonzalez - Guess Game.py
+++ notes/Kimberly Gonzalez - Guess Game.py
@@ -1,6 +1,7 @@
 import random
 a = random.randint(1, 10)
-
+print("guesses left")
+g = 5
 
 guess1 = input("Guess a number between 1 and 10.")
 
@@ -13,3 +14,6 @@ def guess(number):
     else:
         return "Correct Number"
 
+
+your_guess = guess(guess1)
+print(your_guess)
