2476c1e56f11500747ebef64e0547c467fb438fe
Xena774 <42391953+Xena774@users.noreply.github.com>
2018-11-02 17:52:20 -0700

    Worked some more on Guess Game

--- notes/Kimberly Gonzalez - Guess Game.py
+++ notes/Kimberly Gonzalez - Guess Game.py
@@ -1,19 +1,27 @@
 import random
 a = random.randint(1, 10)
-print("guesses left")
-g = 5
+guessesMade = 0
+user_won = False
 
-guess1 = input("Guess a number between 1 and 10.")
+MyName = input("What's you name?")
 
+print("well %s, I am thinking of a number between 1 and 10." % MyName)
 
-def guess(number):
-    if number > a:
-        return "The number is too big."
-    elif number < a:
-        return "The number is too small."
+while guessesMade < 5:
+    guess = input("Guess a number.")
+    guess = int(guess)
+    if guess == a:
+        print("That's the correct number!")
+        user_won = True
+    elif guess > a:
+        print("That number was too high")
+        guessesMade += 1
     else:
-        return "Correct Number"
+        print("That number was too small")
+        guessesMade -= 1
 
-
-your_guess = guess(guess1)
-print(your_guess)
+if user_won:
+    print("You win, %s! Congrats!" % MyName)
+    guessesMade 
+else:
+    print("You lost. Better luck next time. By the way the right number was %d" % a)
