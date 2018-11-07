67ee4719e9a2b82f5a6201feeba96d01209a79fe
Xena774 <42391953+Xena774@users.noreply.github.com>
2018-11-07 14:36:52 -0800

    Started Lucky 7's

--- notes/Kimberly Gonzalez - Guess Game.py
+++ notes/Kimberly Gonzalez - Guess Game.py
@@ -1,15 +1,16 @@
 import random
 a = random.randint(1, 10)
 guessesMade = 0
-guess = 0
+playing = True
 MyName = input("What's you name?")
 
 print("Well, %s, I am thinking of a number between 1 and 10." % MyName)
 
-while a != guess and guessesMade < 5:
+while playing and guessesMade < 5:
     guess = int(input("Guess a number."))
     if guess == a:
         print("That's the correct number!")
+        playing = False
     elif guess > a:
         print("That number was too high")
         guessesMade += 1
@@ -17,7 +18,7 @@ while a != guess and guessesMade < 5:
         print("That number was too small")
         guessesMade += 1
 
-if guess == a:
+if not playing:
     print("Congrats, %s, you guessed the number. Good for you." % MyName)
 else:
     print("You lost. Better luck next time. By the way the right number was %d" % a)
---
+++ notes/Kimberly Gonzalez - Lucky 7s.py
@@ -0,0 +1,9 @@
+import random
+money = 15
+
+MyName = input("What is your name?")
+
+print("Welcome, %s. Your about to play lucky 7" % MyName)
+
+while money > 0:
+    a = random.randint
