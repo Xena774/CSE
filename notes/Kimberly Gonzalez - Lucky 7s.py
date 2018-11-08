94f28b85076ff212279af44de76e66cfb9305568
Xena774 <42391953+Xena774@users.noreply.github.com>
2018-11-08 14:36:16 -0800

    Worked on Lucky 7's

--- notes/Kimberly Gonzalez - Lucky 7s.py
+++ notes/Kimberly Gonzalez - Lucky 7s.py
@@ -1,9 +1,21 @@
 import random
 money = 15
+playing = True
+total_roles = 0
 
-MyName = input("What is your name?")
+while money > 0 and playing:
+    dice_roll1 = random.randint(1, 6)
+    dice_roll2 = random.randint(1, 6)
+    print(dice_roll1, dice_roll2)
+    role = dice_roll1 + dice_roll2
+    if role != 7:
+        money -= 1
+    print("Roll again.")
+    total_roles += 1
+else:
+    money += 5
+    print("You rolled a 7.")
+    total_roles += 1
 
-print("Welcome, %s. Your about to play lucky 7" % MyName)
-
-while money > 0:
-    a = random.randint
+if not playing:
+    print("You're out of money, you played %d and won %d." % (total_roles, money))
