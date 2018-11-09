b2591663010dd72242e6f06147ef4a3718477d2b
Xena774 <42391953+Xena774@users.noreply.github.com>
2018-11-09 14:17:29 -0800

    Finished Lucky 7's

--- notes/Kimberly Gonzalez - Lucky 7s.py
+++ notes/Kimberly Gonzalez - Lucky 7s.py
@@ -3,19 +3,18 @@ money = 15
 playing = True
 total_roles = 0
 
-while money > 0 and playing:
+while playing and money > 0:
     dice_roll1 = random.randint(1, 6)
     dice_roll2 = random.randint(1, 6)
     print(dice_roll1, dice_roll2)
     role = dice_roll1 + dice_roll2
-    if role != 7:
+    if role == 7:
+        money += 5
+        print("You rolled a 7. Nice.")
+        total_roles += 1
+    else:
         money -= 1
-    print("Roll again.")
-    total_roles += 1
-else:
-    money += 5
-    print("You rolled a 7.")
-    total_roles += 1
+        print("Roll again.")
+        total_roles += 1
 
-if not playing:
-    print("You're out of money, you played %d and won %d." % (total_roles, money))
+print("You're out of money, you played %d rounds and won $%d." % (total_roles, money))
