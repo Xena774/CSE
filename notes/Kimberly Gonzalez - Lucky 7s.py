44ffc153a493138207671a24c970d8fd93bf2f01
Xena774 <42391953+Xena774@users.noreply.github.com>
2018-11-13 14:36:58 -0800

    Worked on Lucky 7 extension

--- notes/Kimberly Gonzalez - Lucky 7s.py
+++ notes/Kimberly Gonzalez - Lucky 7s.py
@@ -1,24 +1,28 @@
 import random
-money = 15
+current_money = 15
 playing = True
 total_roles = 0
 top_money = 15
 top_round = 0
 
-while playing and money > 0:
+while playing and current_money > 0:
     dice_roll1 = random.randint(1, 6)
     dice_roll2 = random.randint(1, 6)
     print(dice_roll1, dice_roll2)
     role = dice_roll1 + dice_roll2
     if role == 7:
-        money += 5
+        current_money += 4
         print("You rolled a 7. Nice.")
         total_roles += 1
-        top_money += 5
+        top_money += 4
+        if current_money > top_money:
+            current_money = top_money
+        else:
+            print("Your top money is %d" % top_money)
     else:
-        money -= 1
+        current_money -= 1
         print("Roll again.")
         total_roles += 1
 
-print("You're out of money, you played %d rounds and won $%d." % (total_roles, money))
+print("You're out of money, you played %d rounds and won $%d." % (total_roles, current_money))
 print("Your top money was $%d at round %d." % (top_money, top_round))
