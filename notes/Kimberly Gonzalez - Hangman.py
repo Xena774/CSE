7cb99ea11ffadbeb5b1bae4775ecacb0cf7bd955
Xena774 <42391953+Xena774@users.noreply.github.com>
2018-11-15 14:35:37 -0800

    Worked on Hangman

--- notes/Kimberly Gonzalez - Hangman.py
+++ notes/Kimberly Gonzalez - Hangman.py
@@ -1,2 +1,13 @@
 import random
-words = []
+import string
+print("Welcome to hangman!")
+words = ['hot', 'cold', 'fire', 'ice', 'clock', 'tiger', 'family', 'edison', 'hamburger', 'computer', 'pizza', 'mouse',
+         'winter', 'school', 'child', 'door', 'challenge', 'justice', 'rock', 'chess']
+
+letters = list(string.ascii_lowercase)
+guesses_made = 0
+
+random_word = random.choice(words)
+
+print(letters)
+print(string.punctuation)
