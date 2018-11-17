64e9aa122435c1c9e26b1c92a4fd61091ea7b263
Xena774 <42391953+Xena774@users.noreply.github.com>
2018-11-16 18:11:40 -0800

    Worked on Hangman

--- notes/Kimberly Gonzalez - Hangman.py
+++ notes/Kimberly Gonzalez - Hangman.py
@@ -6,19 +6,26 @@ words = ['hot', 'cold', 'fire', 'ice', 'clock', 'tiger', 'family', 'edison', 'ha
 letters_guesses = []
 letters = list(string.ascii_lowercase)
 guesses_made = 0
-
+playing = True
 random_word = random.choice(words)
 
-while guesses_made < 8:
+while guesses_made < 8 and playing:
     letter = input("Guess a letter")
     if letter in random_word:
         print("You guessed right")
         guesses_made += 1
         letters_guesses.append(letter)
+    elif letters_guesses == random_word:
+        playing = False
     else:
         print("You guessed wrong")
         guesses_made += 1
         letters_guesses.append(letter)
 
-print("The correct letter is %s" % random_word)
+if not playing:
+    print("You guessed the word. Congrats.")
+else:
+    print("You didn't win. Better luck next time.")
+
+print("The correct word is %s" % random_word)
 print("The letters you guessed were %s" % letters_guesses)
