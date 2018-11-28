56c647f5f26ebba6eb3bdbce7848e3bfa933845f
Xena774 <42391953+Xena774@users.noreply.github.com>
2018-11-28 14:37:33 -0800

    Worked on Mad Lib 2

---
+++ notes/Kimberly Gonzalez - Mad Lib Part 2.py
@@ -0,0 +1,21 @@
+words = [
+    input("noun?"),
+    input("adjective?"),
+    input("noun?"),
+    input("noun?"),
+    input("verb ending in ing?"),
+    input("noun?"),
+    input("noun?"),
+    input("plural noun?"),
+    input("plural noun?"),
+    input("plural noun?"),
+]
+print()
+print("Harvard, Yale, %s, Stanford and other colleges around the world." % words[0])
+print("We've all heard of %s and, for some, have wanted to go to them. " % words[1])
+print("Going to %s has been considered necessary to be successful in life." % words[2])
+print("We go to %s to get a job, train for a career and pursue our dreams." % words[3])
+print("There's also the part of having fun and the chance of %s." % words[4])
+print("A lot of people consider %s the highlight of their life." % words[5])
+print("Other %s are not sure that college is worth it for a variety of reasons." % words[6])
+print("People should go to college because it helps you get %s, options, and %s." % (words[7], words[8]))
