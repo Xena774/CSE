c35cc9c2ab5534b6e4d11be27c065826db9dfe43
Xena774 <42391953+Xena774@users.noreply.github.com>
2018-12-03 14:37:24 -0800

    Started Challenges

---
+++ notes/Kimberly Gonzalez - Python Challenges.py
@@ -0,0 +1,12 @@
+def challenge1(firstname, lastname):
+    return "%s %s" % (lastname, firstname)
+
+
+print(challenge1("John", "Doe"))
+
+
+def challenge2(number):
+    return "The number,%d, is odd" % number
+
+
+print(challenge2(97))
