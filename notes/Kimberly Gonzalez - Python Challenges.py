315472d1a15e14e1b399adf86d7daf86e3d3f2a5
Xena774 <42391953+Xena774@users.noreply.github.com>
2018-12-04 14:37:12 -0800

    Worked on Python Challenges

--- notes/Kimberly Gonzalez - Python Challenges.py
+++ notes/Kimberly Gonzalez - Python Challenges.py
@@ -6,7 +6,49 @@ print(challenge1("John", "Doe"))
 
 
 def challenge2(number):
-    return "The number,%d, is odd" % number
+    if number % 2 == 0:
+        return "This number is even"
+    else:
+        return "This number is odd"
 
 
-print(challenge2(97))
+print(challenge2(99999990))
+
+
+def challenge3(b, h):
+    return 1/2 * b * h
+
+
+print(challenge3(4, 2))
+
+
+def challenge4(number):
+    if number == 0:
+        return "This is zero"
+
+
+def challenge5(r):
+    return 3.14 * r ** 2
+
+
+print(challenge5(5))
+
+
+def challenge6(r):
+    return 4/3 * 3.14 * r ** 3
+
+
+print(challenge6(4))
+
+
+vowels = ['a', 'e', 'i', 'o', 'u']
+
+
+def challenge9(letter):
+    if letter in vowels:
+        return "This is a vowel"
+    else:
+        return "This is a constant"
+
+
+print(challenge9("r"))
