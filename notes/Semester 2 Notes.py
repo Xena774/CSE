e23951749609e32a677f6c021514a58ff9274bf3
Xena774 <42391953+Xena774@users.noreply.github.com>
2019-01-08 14:38:18 -0800

    review in notes

---
+++ notes/Semester 2 Notes.py
@@ -0,0 +1,17 @@
+print("Hello World!")
+# quoth the Raven "Nevermore"
+
+cars = 5
+driving = True
+
+print("I have %d cars" % cars)
+print("I have " + str(cars) + " cars.")  # concatenate
+
+age = input("How old are you?")
+print("%s?? Really???" % age)
+
+
+colors = ['blue', 'purple', 'pink', 'red', 'orange']
+colors.append("magenta")
+
+print(colors)
