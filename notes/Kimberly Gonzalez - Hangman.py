9fa657fdc1ad1b465ee461282f5bb5e6e0329878
Xena774 <42391953+Xena774@users.noreply.github.com>
2019-01-15 14:36:05 -0800

    hangman

--- notes/Kimberly Gonzalez - Hangman.py
+++ notes/Kimberly Gonzalez - Hangman.py
@@ -1,23 +1,26 @@
 import random
 print("Welcome to hangman!")
 words = ['hot', 'cold', 'fire', 'ice', 'clock', 'tiger', 'family', 'edison', 'hamburger', 'computer', 'pizza', 'mouse',
-         'winter', 'school', 'child', 'Goodbye', 'change', 'justice', 'rock', 'check?']
-letters_guesses = []
+         'winter', 'school', 'child', 'Goodbye', 'change', 'justice', 'rock', 'hello?']
+letters_guessed = []
 letters_guessed_right = []
 guesses_made = 0
 playing = True
 random_word = random.choice(words)
+word = str(random_word)
+
 
 while guesses_made < 8 and playing:
     letter = input("Guess a letter")
-    if letter in random_word and letter not in letters_guesses:
+    if letter in random_word and letter not in letters_guessed:
         print("You guessed right")
-        if letter not in letters_guesses:
+        if letter not in letters_guessed:
+            guesses_made += 1
             letters_guessed_right.append(letter)
             print("Number of guesses made so far: %d" % guesses_made)
             if random_word != letters_guessed_right and len(letters_guessed_right) == len(random_word):
                 playing = False
-            elif word == letters_guessed_right and len(letters_guessed_right) == len(random_word):
+            elif random_word == letters_guessed_right and len(letters_guessed_right) == len(random_word):
                 playing = False
         else:
             print("You already guessed this letter")
@@ -33,6 +36,6 @@ else:
 
 print("The correct word is %s." % random_word)
 print("The letters right were:")
-print("".join(letters_guesses))
+print("".join(letters_guessed))
 print("The letters right were:")
 print("" .join(letters_guessed_right))
