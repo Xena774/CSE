4c9a5222fb275550ee91956a1287dbf115e6bcd6
Xena774 <42391953+Xena774@users.noreply.github.com>
2019-02-26 14:39:25 -0800

    made player and controller

--- notes/Kimberly Gonzalez - World Map OOP.py
+++ notes/Kimberly Gonzalez - World Map OOP.py
@@ -6,19 +6,45 @@ class Room(object):
         self.south = south
         self.east = east
         self.west = west
+
+
+class Player(object):
+    def __init__(self, starting_location):
+        self.health = 100
+        self.inventory = []
+        self.current_location = starting_location
+
+    def move(self, new_location):
+        """This method moves a player to a new location
+
+        :param new_location: The room object that we move to
+        """
+        self.current_location = new_location
+
+    def find_room(self, direction):
+        """This method takes a direction, and finds the variable of the room.
+
+        :param direction: A String (all lowercase), with a cardinal direction
+        :return: A room object if it exists, None if it does not
+        """
+        room_name = getattr(self.current_location, direction)
+        return globals()[room_name]
+
+
 # These are the instances of the room(Instantiation)
 
-
+""""
 # Option 1 - Use the variables, but fix later
 R19A = Room("Mr. Wiebe's Room", None)
 parking_lot = Room("The Parking Lot", None, R19A)
 
 
 R19A.north = parking_lot
-
+"""
 # Option 2 - Use Strings, but more difficult controller
 R19A = Room("Mr. Wiebe's Room", 'parking_lot')
 parking_lot = Room("The Parking Lot", None, "R19A")
+
 
 # My own
 
@@ -93,3 +119,26 @@ Great_Wall = Room("Great Wall of China", "The great wall has been around for for
 Australia = Room("Australia", "Welcome to Australia! This place is beautiful and the accents are amazing. There's also"
                  "the great surfing competitions that take place here. This place is also where multiple places "
                  "sent their prisoners. You have found the lost gold reef. Congrats!", "New_Delhi")
+
+directions = ['north', 'south', 'east', 'west', 'up', 'down']
+playing = True
+player = Player(Fresno)
+print("Welcome to Around the World. In this game you travel to different places. Sometimes it's a city, country,"
+      "state. The goal is to collect things in different places of the world. You have to collect every treasure to"
+      " win the game. Good luck!")
+
+# Controller
+while playing:
+    print(player.current_location.name)
+    print(player.current_location.description)
+    command = input(">_")
+    if command.lower() in ['q', 'quit', 'exit']:
+        playing = False
+    elif command in directions:
+        try:
+            next_room = player.find_room(command)
+            player.move(next_room)
+        except KeyError:
+            print("I can't go that way")
+    else:
+        print("Command not recognized.")
