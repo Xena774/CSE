d4d7f95680589267580a9b33b89f6fe47d472b32
Xena774 <42391953+Xena774@users.noreply.github.com>
2019-05-08 14:39:17 -0700

    making progress

--- notes/Kimberly Gonzalez - Sales Record.py
+++ notes/Kimberly Gonzalez - Sales Record.py
@@ -1,9 +1,31 @@
 import csv
 
+profit_list = []
+
+item_profit = {
+    'Fruits': {
+        'Entry': 0
+    },
+    'Clothes': {
+        'Entry': 0
+    },
+    'Meat': {
+        'Entry': 0
+    },
+    'Beverages': {
+        'Entry': 0
+    }
+}
+
 
 with open("Sales Records.csv", 'r') as old_csv:
     reader = csv.reader(old_csv)
     for row in reader:
+        if row[13] == "Total Profit":
+            continue
         item = row[2]
-        profit = row[13]
-        print(item)
+        profit = float(row[13])
+        if "Fruits" == item:
+            profit_list.append(profit)
+            total_profit = sum(profit_list)
+    print(total_profit)
