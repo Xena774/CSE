faa327b9582065172882b34b3d6de1f262e1060c
Xena774 <42391953+Xena774@users.noreply.github.com>
2019-05-10 14:49:55 -0700

    finished first extention

--- notes/Kimberly Gonzalez - Sales Record.py
+++ notes/Kimberly Gonzalez - Sales Record.py
@@ -1,7 +1,10 @@
 import csv
 
 item_profit = {}
-top_profit = 0
+region_profit = {}
+top_item_profit = 0
+top_region_profit = 0
+
 
 with open("Sales Records.csv", 'r') as old_csv:
     reader = csv.reader(old_csv)
@@ -10,9 +13,26 @@ with open("Sales Records.csv", 'r') as old_csv:
             continue
         item = row[2]
         Indv_profit = float(row[13])
+        region = row[0]
+
         try:
             item_profit[item] += Indv_profit
+            region_profit[region] += Indv_profit
+
         except KeyError:
             item_profit[item] = Indv_profit
-            
-    print(item_profit)
+            region_profit[region] = Indv_profit
+
+        if item_profit[item] > top_item_profit:
+            top_item_profit = item_profit[item]
+
+        if region_profit[region] > top_region_profit:
+            top_region_profit = region_profit[region]
+
+    for key, value in item_profit.items():
+        if value == top_item_profit:
+            print(key)
+
+    for key, value in region_profit.items():
+        if value == top_region_profit:
+            print(key)
