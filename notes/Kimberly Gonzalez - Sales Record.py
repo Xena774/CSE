e05052025b8d28e90b370b5ce6895c0a7494c4bb
Xena774 <42391953+Xena774@users.noreply.github.com>
2019-05-14 14:38:25 -0700

    worked on last extension

--- notes/Kimberly Gonzalez - Sales Record.py
+++ notes/Kimberly Gonzalez - Sales Record.py
@@ -2,9 +2,10 @@ import csv
 
 item_profit = {}
 region_profit = {}
-units = {}
+units_dict = {}
 top_item_profit = 0
 top_region_profit = 0
+units = 0
 
 with open("Sales Records.csv", 'r') as old_csv:
     reader = csv.reader(old_csv)
@@ -14,22 +15,26 @@ with open("Sales Records.csv", 'r') as old_csv:
         item = row[2]
         Indv_profit = float(row[13])
         region = row[0]
-        units_sold = float(row[9])
+        units_sold = int(row[8])
 
         try:
             item_profit[item] += Indv_profit
             region_profit[region] += Indv_profit
+            units_dict[item] += units
 
         except KeyError:
             item_profit[item] = Indv_profit
             region_profit[region] = Indv_profit
-            
+            units_dict[item] = units_sold
 
         if item_profit[item] > top_item_profit:
             top_item_profit = item_profit[item]
 
         if region_profit[region] > top_region_profit:
             top_region_profit = region_profit[region]
+
+        if item_profit[item]/units_dict[item] > units:
+            units = item_profit[item]
 
     for key, value in item_profit.items():
         if value == top_item_profit:
@@ -39,3 +44,6 @@ with open("Sales Records.csv", 'r') as old_csv:
         if value == top_region_profit:
             print(key)
 
+    for key, value in units_dict.items():
+        if item_profit[item]/value == units:
+            print(key)
